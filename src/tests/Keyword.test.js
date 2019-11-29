91c4245a99c23618cffdf2dc87d478e126bacd0d
Usman Zaheer <usmanzaheer1995@gmail.com>
2019-11-29 08:18:46 +0500

    Added test file for snapshot testing

---
+++ src/tests/Keyword.test.js
@@ -0,0 +1,14 @@
+// Have test case implemented (simple snapshot testing is sufficient)
+
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import Keyword from '../components/Keyword';
+
+test('should render Keyword correctly', () => {
+    const wrapper = shallow(<Keyword keyword={"Test keyword"}/>);
+    expect(wrapper.find('li').length).toBe(1);
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Keyword keyword={"Test keyword"}/>);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
